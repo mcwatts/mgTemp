dca742402de6dfd6c8b05d86492b1c12caee95d2
Maddie Watts <72463771+mcwatts@users.noreply.github.com>
2021-10-23 12:43:11 -0700

    Update parentController.cs

--- newFile/parentController.cs
+++ newFile/parentController.cs
@@ -9,11 +9,11 @@ public class parentController : MonoBehaviour
      void Update{
           if(Input.getKeyDown(KeyCode.UpArrow){
               //move display up
-              
+              parent.transform.position = new Vector(0,0.1,0);
           }
           if(Input.getKeyDown(KeyCode.DownArrow){
               //move display down
-              
+              parent.transform.position = new Vector(0,-0.1,0);
           }
           
      }
