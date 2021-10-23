2a61671496e95d8dcd922d70ba77f33c138a22c5
Maddie Watts <72463771+mcwatts@users.noreply.github.com>
2021-10-23 12:35:24 -0700

    Update parentController.cs

--- newFile/parentController.cs
+++ newFile/parentController.cs
@@ -1,1 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class parentController : MonoBehaviour
+{
+     public GameCube parent;
+     
+     void Update{
+          if(Input.getKeyDown(KeyCode.UpArrow){
+              //move display up
+              
+          }
+          if(Input.getKeyDown(KeyCode.DownArrow){
+              //move display down
+              
+          }
+          
+     }
+}
